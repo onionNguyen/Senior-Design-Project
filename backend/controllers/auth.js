2711d7df2b866952970926729efb04766b4aef22
Anthony Ngo <anthonyhtngo@gmail.com>
2023-10-18 18:42:05 -0500

    Modify auth controller to support updated database schema

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,13 +3,45 @@ const bcrypt = require("bcrypt");
 const db = require("../db");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const {
+    first_name,
+    middle_name,
+    last_name,
+    mobile_phone,
+    work_phone,
+    home_phone,
+    email,
+    password,
+    country_name,
+  } = req.body;
 
   try {
     const encryptedPassword = bcrypt.hashSync(password, 8);
+    const country_result = await db.query(
+      'SELECT id FROM "uARexpert_test_schema_001".country WHERE name = $1',
+      [country_name]
+    );
+    country_id = country_result.rows[0].id;
     const result = await db.query(
-      'INSERT INTO se4485_test_schema.users (name, email, password) VALUES ($1, $2, $3) RETURNING "ID"',
-      [name, email, encryptedPassword]
+      'INSERT INTO "uARexpert_test_schema_001".user (first_name, middle_name, last_name, mobile_phone, work_phone, home_phone, email, password, country_id, role_id, status_id, created_on, created_by, modified_on, modified_by, row_version) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING id',
+      [
+        first_name,
+        middle_name ? middle_name : null,
+        last_name,
+        mobile_phone,
+        work_phone ? work_phone : null,
+        home_phone ? home_phone : null,
+        email,
+        encryptedPassword,
+        country_id,
+        1,
+        1,
+        generateDatabaseDateTime(new Date()),
+        1,
+        generateDatabaseDateTime(new Date()),
+        1,
+        1,
+      ]
     );
     const userId = result.rows[0].ID;
     res.status(200).json({ message: "User registered successfully", userId });
@@ -24,7 +56,7 @@ const login = async (req, res) => {
 
   try {
     const result = await db.query(
-      "SELECT * FROM se4485_test_schema.users WHERE email = $1",
+      'SELECT * FROM "uARexpert_test_schema_001".user WHERE email = $1',
       [email]
     );
     const user = result.rows[0];
@@ -61,6 +93,10 @@ const login = async (req, res) => {
     console.error("Error logging in user:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
+};
+
+const generateDatabaseDateTime = (date) => {
+  return date.toISOString().replace("T", " ").substring(0, 19);
 };
 
 module.exports = { register, login };
