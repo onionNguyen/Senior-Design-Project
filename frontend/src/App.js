d8e325842dada6c398decdaaa4b11a3230752f1e
Anthony Ngo <anthonyhtngo@gmail.com>
2023-10-18 15:17:30 -0500

    Move React.js application to frontend directory

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Footer from "./components/footer/Footer";
+import Register from "./components/pages/registration/Register";
+
+function App() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
