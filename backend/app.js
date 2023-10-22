c7a6c00bc0316ffac2f1833801b8be68a616124f
Anthony Ngo <anthonyhtngo@gmail.com>
2023-10-21 21:43:54 -0500

    Change backend server port from 3000 to 5000

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,6 @@ app.use(authRoutes);
 app.use(countryRoutes);
 app.use(languageRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is live on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is live on port 5000");
 });
