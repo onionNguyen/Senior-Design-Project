67105d52ac9d4c4c077cc5e445d3c9f0b53be57c
Anthony Ngo <anthonyhtngo@gmail.com>
2023-10-26 21:26:58 -0500

    Add logout functionality

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./Header.module.css";
 import mainLogo from "../../assets/logo.png";
+import { useContext } from "react";
+import { AuthContext } from "../../helpers/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const { authState, setAuthState } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState(false);
+    navigate("/login");
+  };
+
   return (
     <div className={styles["header"]}>
       <h1>uARexpert</h1>
@@ -11,6 +23,7 @@ const Header = () => {
           <img src={mainLogo} alt="Logo" />
         </div>
       </div>
+      {authState && <button onClick={logout}>Logout</button>}
     </div>
   );
 };
